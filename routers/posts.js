b073716944e248da781b21930dcfadc150b4dea3
ManuelGraziosi <manuel.graziosi@gmail.com>
2026-01-05 19:29:20 +0100

    feat: impostato SHOW: aggiunto anche corretta gestione stato risposta in caso di errore

--- routers/posts.js
+++ routers/posts.js
@@ -24,14 +24,14 @@ postRouter.get("/", (req,res) => {
    if(titolo !== undefined){
       filteredPost = filteredPost.filter((curPost)=> curPost.titolo.toLowerCase().includes(titolo.toLowerCase()))
    }
-   // console.log("filteredPost",filteredPost)
+   
    if(contenuto !== undefined){
       filteredPost = filteredPost.filter((curPost)=> curPost.contenuto.toLowerCase().includes(contenuto.toLowerCase()))
    }
+   
    // if(tags !== undefined){
    //    filteredPost = filteredPost.filter((curPost)=> curPost.tags.filter((curTag) => {curTag.toLowerCase().includes(tags)}))
    // }
-   
    
    const dataResponse = {
       "info" : {
@@ -47,9 +47,18 @@ postRouter.get("/", (req,res) => {
 
 // Show -> Get
 postRouter.get("/:id", (req, res) => {
-   const {id} = req.params;
-   const dataResponse = dataPost.filter((curPost) => curPost.id === parseInt(id))
-   res.json(...dataResponse)
+   const id = parseInt(req.params.id);
+   const dataResponse = dataPost.find((curPost) => curPost.id === id)
+
+   if(!dataResponse){
+      res.status(404)
+      res.json({
+         "error" : "Not Found",
+         "message" : "Post non trovato"
+      })
+   } else{
+      res.json(dataResponse)
+   }
 
 })
 
