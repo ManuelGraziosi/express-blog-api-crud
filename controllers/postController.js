067aeadc01e24a40455ef89774ec70aadb5a4e7d
ManuelGraziosi <manuel.graziosi@gmail.com>
2026-01-06 13:39:07 +0100

    feat: implementato sistema controller

---
+++ controllers/postController.js
@@ -0,0 +1,87 @@
+import dataPost from "../src/dataPosts.js";
+
+const index = (req, res) => {
+  const { titolo, contenuto, tags } = req.query;
+  // console.log(`id: ${id}`);
+  // console.log(`contenuto: ${contenuto}`);
+  // console.log(`immagine: ${immagine}`);
+  // console.log(`tags: ${tags}`);
+  // console.log(`titolo: ${titolo}`);
+
+  let filteredPost = dataPost;
+
+  if (titolo !== undefined) {
+    filteredPost = filteredPost.filter((curPost) =>
+      curPost.titolo.toLowerCase().includes(titolo.toLowerCase())
+    );
+  }
+
+  if (contenuto !== undefined) {
+    filteredPost = filteredPost.filter((curPost) =>
+      curPost.contenuto.toLowerCase().includes(contenuto.toLowerCase())
+    );
+  }
+
+  // if(tags !== undefined){
+  //    filteredPost = filteredPost.filter((curPost)=> curPost.tags.filter((curTag) => {curTag.toLowerCase().includes(tags)}))
+  // }
+
+  const dataResponse = {
+    info: {
+      lenght: filteredPost.length,
+    },
+    data: {
+      posts: filteredPost,
+    },
+  };
+
+  res.json(dataResponse);
+};
+
+const show = (req, res) => {
+  const id = parseInt(req.params.id);
+  const dataResponse = dataPost.find((curPost) => curPost.id === id);
+
+  if (!dataResponse) {
+    res.status(404);
+    res.json({
+      error: "Not Found",
+      message: "Post non trovato",
+    });
+  } else {
+    res.json(dataResponse);
+  }
+};
+
+const store = (req, res) => {
+  res.send(`Creo Post`);
+};
+
+const update = (req, res) => {
+  const { id } = req.params;
+  res.send(`Aggiorno completamente Post: ${id}`);
+};
+
+const modify = (req, res) => {
+  const { id } = req.params;
+  res.send(`Aggiorno parzialmente Post: ${id}`);
+};
+
+const destroy = (req, res) => {
+  const id = parseInt(req.params.id);
+  //Trovare il post cercato se esiste
+  const post = dataPost.find((curPost) => curPost.id === id);
+
+  if (post === undefined) {
+    res.status(404);
+    res.json({
+      err: "Not Found",
+      message: "Post non trovato",
+    });
+  } else {
+    dataPost.splice(dataPost.indexOf(post), 1);
+    res.sendStatus(204);
+  }
+};
+
+export default { index, show, store, update, modify, destroy };
--- routers/posts.js
+++ routers/posts.js
@@ -1,4 +1,4 @@
-import dataPost from "../src/dataPosts.js";
+import postController from "../controllers/postController.js";
 import express from "express";
 
 const postRouter = express.Router();
@@ -10,92 +10,22 @@ const postRouter = express.Router();
  */
 
 // Index -> Get (prende anche le query string param)
-postRouter.get("/", (req,res) => {
-
-   const {titolo, contenuto, tags} = req.query;
-   // console.log(`id: ${id}`);
-   // console.log(`contenuto: ${contenuto}`);
-   // console.log(`immagine: ${immagine}`);
-   // console.log(`tags: ${tags}`);
-   // console.log(`titolo: ${titolo}`);
-   
-   let filteredPost = dataPost;
-   
-   if(titolo !== undefined){
-      filteredPost = filteredPost.filter((curPost)=> curPost.titolo.toLowerCase().includes(titolo.toLowerCase()))
-   }
-   
-   if(contenuto !== undefined){
-      filteredPost = filteredPost.filter((curPost)=> curPost.contenuto.toLowerCase().includes(contenuto.toLowerCase()))
-   }
-   
-   // if(tags !== undefined){
-   //    filteredPost = filteredPost.filter((curPost)=> curPost.tags.filter((curTag) => {curTag.toLowerCase().includes(tags)}))
-   // }
-   
-   const dataResponse = {
-      "info" : {
-         "lenght" : filteredPost.length,
-      },
-      "data" : {
-         "posts" : filteredPost,
-      }
-   }
-
-   res.json(dataResponse)
- })
+postRouter.get("/", postController.index)
 
 // Show -> Get
-postRouter.get("/:id", (req, res) => {
-   const id = parseInt(req.params.id);
-   const dataResponse = dataPost.find((curPost) => curPost.id === id)
-
-   if(!dataResponse){
-      res.status(404)
-      res.json({
-         "error" : "Not Found",
-         "message" : "Post non trovato"
-      })
-   } else{
-      res.json(dataResponse)
-   }
-
-})
+postRouter.get("/:id", postController.show)
 
 // Store -> Post
-postRouter.post("/", (req, res) => {
-   res.send(`Creo Post`)
-})
+postRouter.post("/", postController.store)
 
 // Update -> Put
-postRouter.put("/:id", (req, res) => {
-   const {id} = req.params;
-   res.send(`Aggiorno completamente Post: ${id}`)
-})
+postRouter.put("/:id", postController.update)
 
 // Modify -> Patch
-postRouter.patch("/:id", (req, res) => {
-   const {id} = req.params;
-   res.send(`Aggiorno parzialmente Post: ${id}`)
-})
+postRouter.patch("/:id", postController.modify)
 
  // Destroy -> Delete
-postRouter.delete("/:id", (req, res) => {
-   const id = parseInt(req.params.id);
-   //Trovare il post cercato se esiste
-   const post = dataPost.find((curPost) => curPost.id === id);
-
-   if (post === undefined){
-      res.status(404);
-      res.json({
-         "err" : "Not Found",
-         "message" : "Post non trovato"
-      })
-   }else{
-      dataPost.splice(dataPost.indexOf(post), 1)
-      res.sendStatus(204);
-   }
-})
+postRouter.delete("/:id", postController.destroy)
 
 
 export default postRouter;
